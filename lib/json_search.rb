566668bb3d713af5396c280b6603d07807cfda64
kanmani <kanmaniselvan@gmail.com>
2017-12-30 10:00:53 +0530

    Add methods to perform the search on the JSON and return the results
    * The input string will be appended with `||` and will be queried for `like` presence in the db.

--- lib/json_search.rb
+++ lib/json_search.rb
@@ -1,7 +1,14 @@
 require './db/connection'
-require 'activerecord-import'
 
 class JsonSearch < ActiveRecord::Base
   validates :search_values, :json_data, :json_data_type, presence: true
   validates_uniqueness_of :search_values
+
+  def self.search(keyword)
+    JsonSearch.where('search_values like ?', "%#{get_separated_joined_keyword(keyword)}%")
+  end
+
+  def self.get_separated_joined_keyword(keyword)
+    "#{::JsonIndex::VALUES_SEPARATOR}#{keyword}#{::JsonIndex::VALUES_SEPARATOR}"
+  end
 end
