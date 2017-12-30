8c616652bf82aa6c6fd545edb87c2fbcebf723be
kanmani <kanmaniselvan@gmail.com>
2017-12-30 09:08:32 +0530

    Read all the JSON files on initialize
    * Add method to check for json file type and take json file name. 
    * File will be read and all these information will be stored as a hash on initializing the index.

--- lib/json_index.rb
+++ lib/json_index.rb
@@ -7,6 +7,23 @@ class JsonIndex
   JSON_FILES_PATH = './json_files/*.json'
 
   def initialize
-    @files = Dir[JSON_FILES_PATH].each { |file| File.open(file) }
+    @files = Dir[JSON_FILES_PATH].map do |file_path|
+
+      unless is_json_type(file_path)
+        raise "Invalid File - #{file_path}. It should be JSON type."
+      end
+
+      { file: File.read(file_path),
+        path: file_path,
+        name: json_file_name(file_path) }
+    end
+  end
+
+  def is_json_type(file)
+    '.json' == File.extname(file)
+  end
+
+  def json_file_name(file)
+    File.basename(file).gsub(/\.json/, '')
   end
 end
